d4478f8820670f10f452f714cac818bf9c1047d7
Salvatore Gonda <salvatore.gonda@gmail.com>
2023-02-21 10:07:04 +0100

    added behavioral pattern mediator

---
+++ design-patterns/BehavioralDesignPatterns/Mediator/README.md
@@ -0,0 +1,5 @@
+# Mediator
+
+Mediator ist ein Verhaltensmuster, das die Kopplung zwischen den Komponenten eines Programms reduziert, indem es sie indirekt über ein spezielles Mediator-Objekt kommunizieren lässt.
+
+Der Mediator erleichtert das Ändern, Erweitern und Wiederverwenden einzelner Komponenten, da sie nicht mehr von den Dutzenden anderer Klassen abhängig sind.
---
+++ design-patterns/BehavioralDesignPatterns/Mediator/example1/main.go
@@ -0,0 +1,124 @@
+package main
+
+import "fmt"
+
+/*
+	Ein hervorragendes Beispiel für das Vermittlungsmuster ist ein Bahnhofsverkehrssystem.
+	Zwei Züge kommunizieren untereinander nie über die Verfügbarkeit des Bahnsteigs.
+	Der stationManager fungiert als Vermittler und stellt den Bahnsteig
+	nur einem der ankommenden Züge zur Verfügung, während er die übrigen
+	in einer Warteschlange hält.
+	Ein abfahrender Zug benachrichtigt den Bahnhof, der den nächsten Zug in der
+	Warteschlange ankommen lässt.
+*/
+
+// Component
+
+type Train interface {
+	arrive()
+	depart()
+	permitArrival()
+}
+
+// Concrete component
+
+type PassengerTrain struct {
+	mediator Mediator
+}
+
+func (g *PassengerTrain) arrive() {
+	if !g.mediator.canArrive(g) {
+		fmt.Println("PassengerTrain: Arrival blocked, waiting")
+		return
+	}
+	fmt.Println("PassengerTrain: Arrived")
+}
+
+func (g *PassengerTrain) depart() {
+	fmt.Println("PassengerTrain: Leaving")
+	g.mediator.notifyAboutDeparture()
+}
+
+func (g *PassengerTrain) permitArrival() {
+	fmt.Println("PassengerTrain: Arrival permitted, arriving")
+	g.arrive()
+}
+
+type FreightTrain struct {
+	mediator Mediator
+}
+
+func (g *FreightTrain) arrive() {
+	if !g.mediator.canArrive(g) {
+		fmt.Println("FreightTrain: Arrival blocked, waiting")
+		return
+	}
+	fmt.Println("FreightTrain: Arrived")
+}
+
+func (g *FreightTrain) depart() {
+	fmt.Println("FreightTrain: Leaving")
+	g.mediator.notifyAboutDeparture()
+}
+
+func (g *FreightTrain) permitArrival() {
+	fmt.Println("FreightTrain: Arrival permitted")
+	g.arrive()
+}
+
+// Mediator Interface
+
+type Mediator interface {
+	canArrive(Train) bool
+	notifyAboutDeparture()
+}
+
+// Concrete Mediator
+
+type StationManager struct {
+	isPlatformFree bool
+	trainQueue     []Train
+}
+
+func newStationManger() *StationManager {
+	return &StationManager{
+		isPlatformFree: true,
+	}
+}
+
+func (s *StationManager) canArrive(t Train) bool {
+	if s.isPlatformFree {
+		s.isPlatformFree = false
+		return true
+	}
+	s.trainQueue = append(s.trainQueue, t)
+	return false
+}
+
+func (s *StationManager) notifyAboutDeparture() {
+	if !s.isPlatformFree {
+		s.isPlatformFree = true
+	}
+	if len(s.trainQueue) > 0 {
+		firstTrainInQueue := s.trainQueue[0]
+		s.trainQueue = s.trainQueue[1:]
+		firstTrainInQueue.permitArrival()
+	}
+}
+
+// Client code
+
+func main() {
+	stationManager := newStationManger()
+
+	passengerTrain := &PassengerTrain{
+		mediator: stationManager,
+	}
+	freightTrain := &FreightTrain{
+		mediator: stationManager,
+	}
+
+	passengerTrain.arrive()
+	freightTrain.arrive()
+	passengerTrain.depart()
+}
